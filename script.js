2ca79c414aa5feebe8cdb0bf0e8d2a92450396c2
TonicGaro <103894046+TonicGaro@users.noreply.github.com>
2023-03-01 16:19:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log('hey smily :)');
 let susbtn = document.getElementById('among')
 let susbtnclass = document.querySelector('.among')
 
+susbtn.onclick = susload()
+
 function susload() {
   susbtn.innerHTML = `Loading ...`
   susbtnclass.style.cursor = 'default';
