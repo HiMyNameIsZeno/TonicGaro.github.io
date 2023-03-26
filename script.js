238b2e5d714608fe44dee1088718e7ef14ab8a04
TonicGaro <103894046+TonicGaro@users.noreply.github.com>
2023-03-25 20:52:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-window.onload = function() {
+console.log('hey smily :)');
 
-  console.log('hey smily :)');
+const susbtn = document.getElementById('among');
+const susbtnclass = document.querySelector('.among');
 
-  const susbtn = document.getElementById('among');
-  const susbtnclass = document.querySelector('.among');
-
-  function susload() {
-    susbtn.innerHTML = `Loading ...`
-    susbtnclass.style.cursor = 'default';
-    window.location.replace('/among.html');
-  }
-
-  susbtn.addEventListener('click', susload);
+function susload() {
+  susbtn.innerHTML = `Loading ...`
+  susbtnclass.style.cursor = 'default';
+  window.location.replace('/among.html');
 }
+
+susbtn.addEventListener('click', susload);
