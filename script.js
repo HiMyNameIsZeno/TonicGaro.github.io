34f2a297c3630d981ae2de7d3a16e007a0762dbc
TonicGaro <103894046+TonicGaro@users.noreply.github.com>
2023-03-25 21:03:00 -0400

    added comments

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-console.log('hey smily :)');
+// logs easter egg in console
+console.info('%chey smily :)', 'color: #20c20e');
 
+// consts for grabbing element ids / classnames
 const susbtn = document.getElementById('among');
 const susbtnclass = document.querySelector('.among');
 
+// functions
+
+// function to load among us
 function susload() {
   susbtn.innerHTML = `Loading ...`
   susbtnclass.style.cursor = 'not-allowed';
   window.location.replace('/among.html');
 }
 
+// click listeners for btns, runs their respective functions
 susbtn.addEventListener('click', susload);
