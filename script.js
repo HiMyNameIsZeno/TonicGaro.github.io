bebcf81111b29c3373a946c6c739c27b4ba0b355
TonicGaro <103894046+TonicGaro@users.noreply.github.com>
2023-04-02 17:46:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ console.info('%chey smily :)', 'color: #20c20e');
 // consts for grabbing element ids / classnames
 const susbtn = document.getElementById('among');
 const alienbtn = document.getElementById('alien');
+const dadbtn = document.getElementById('dadnme');
 
 // functions
 function susload() {
@@ -18,6 +19,13 @@ function alienload() {
   window.location.replace('/games/alienhominid.html');
 }
 
+function dadload() {
+  dadbtn.innerHTML = `Loading ...`
+  dadbtn.style.cursor = 'not-allowed'
+  window.location.replace('/games/dadnme.html');
+}
+
 // click listeners for btns, runs their respective functions
 susbtn.addEventListener('click', susload);
 alienbtn.addEventListener('click', alienload);
+dadbtn.addEventListener('click', dadload);
