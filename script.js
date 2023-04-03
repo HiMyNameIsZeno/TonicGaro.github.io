af2faf5b6be3c6b2689887b2fbef27e394e4db62
TonicGaro <103894046+TonicGaro@users.noreply.github.com>
2023-04-02 21:28:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 // logs easter egg in console
 console.info('%chey smily :)', 'color: #20c20e');
 
-// consts for grabbing element ids / classnames
-const susbtn = document.getElementById('among');
-const alienbtn = document.getElementById('alien');
-const dadbtn = document.getElementById('dadnme');
-const meatbtn = document.getElementById('meatboy');
-
-// functions
-function susload() {
-  susbtn.innerHTML = `Loading ...`
-  susbtn.style.cursor = 'not-allowed'
-  window.location.replace('/games/among.html');
-}
-
-// click listeners for btns, runs their respective functions
-susbtn.addEventListener('click', susload);
-alienbtn.addEventListener('click', function() { window.location.href = '/games/alienhominid.html' });
-dadbtn.addEventListener('click', function() { window.location.href = '/games/dadnme.html' });
-meatbtn.addEventListener('click', function() { window.location.href = '/games/meatboy.html' });
+// click listeners for btns, directs to games
+document.getElementById('among').addEventListener('click', function() { window.location.href = '/games/among.html' });
+document.getElementById('alien').addEventListener('click', function() { window.location.href = '/games/alienhominid.html' });
+document.getElementById('dadnme').addEventListener('click', function() { window.location.href = '/games/dadnme.html' });
+document.getElementById('meatboy').addEventListener('click', function() { window.location.href = '/games/meatboy.html' });
