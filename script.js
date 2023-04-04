1bdb39d4eb0b6b2d27d91b1598ba6c29e1151708
TonicGaro <103894046+TonicGaro@users.noreply.github.com>
2023-04-04 11:17:05 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// logs easter egg in console
-console.info('%chey smily :)', 'color: #20c20e');
-
-// click listeners for btns, directs to games
-document.getElementById('among').addEventListener('click', function() { window.location.href = '/games/among.html' });
-document.getElementById('alien').addEventListener('click', function() { window.location.href = '/games/alienhominid.html' });
-document.getElementById('dadnme').addEventListener('click', function() { window.location.href = '/games/dadnme.html' });
-document.getElementById('meatboy').addEventListener('click', function() { window.location.href = '/games/meatboy.html' });
